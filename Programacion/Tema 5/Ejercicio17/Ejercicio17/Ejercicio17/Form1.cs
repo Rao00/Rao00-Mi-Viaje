f6c9bbc5dc5bf98b84b9529c64f134ebef4d2bff
Rao00 <carlesmorales.mcv00@gmail.com>
2024-01-08 20:51:49 +0100

    a

--- Programacion/Tema 5/Ejercicio17/Ejercicio17/Ejercicio17/Form1.cs
+++ Programacion/Tema 5/Ejercicio17/Ejercicio17/Ejercicio17/Form1.cs
@@ -19,6 +19,8 @@ namespace Ejercicio17
             //Relaciona el metodo de manejo de eventos con cada TextBox de la seleccion de lineas a operar//
             txtFila1.TextChanged += OperacionesFilas_TextChanged;
             txtFila2.TextChanged += OperacionesFilas_TextChanged;
+            txtFila1.Click += OperacionesFilas_TextChanged;
+            txtFila2.Click += OperacionesFilas_TextChanged;
         }
 
         //------------------------------Parametros Globales------------------------------//
@@ -42,8 +44,14 @@ namespace Ejercicio17
         /// <returns>Devuelve la Matriz bidimensional con la Matriz unidimensional guardada</returns>
         void EscribirArray(int[,] array, int numArray, int[,] ArrayToWrite)
         {
-            for (int i = 0; i < TAMX; i++)
-                ArrayToWrite[numArray, i] = array[0, i];
+            try
+            {
+                for (int i = 0; i < TAMX; i++)
+                    ArrayToWrite[numArray, i] = array[0, i];
+                MessageBox.Show($"Matriz de {TAMX} numeros ha sido creado con exito!");
+            }
+            catch { MessageBox.Show("Linea no valida"); }
+            
         }
         /// <summary>
         /// Funciona de busqueda del valor mas alto de una matriz unidimensional
@@ -167,33 +175,38 @@ namespace Ejercicio17
         /// <returns>Imprime por pantalla el contenido del array completo o el contenido de la fila en cuestion</returns>
         void LeerArray(int[,] array, bool SelectRow = false, int row = 0)
         {
-            if (array[row, 0] != 0)
+            try
             {
-                string salida = "";
-                if (SelectRow)
+                if (array[row, 0] != 0)
                 {
-                    for (int i = 0; i < TAMX-1; i++)
+                    string salida = "";
+                    if (SelectRow)
                     {
-                        salida += array[row, i] + ", ";
-                    }
-                    salida += array[row, TAMX - 1];
-                }
-                else if (!SelectRow)
-                {
-                    for (int i = 1; i < TAMY; i++)
-                    {
-                        salida += $"{i}.- ";
-                        for (int j = 0; j < TAMX-1; j++)
+                        for (int i = 0; i < TAMX - 1; i++)
                         {
-                            salida += array[i, j] + ", ";
+                            salida += array[row, i] + ", ";
                         }
-                        salida += $"{array[i, TAMX - 1]}\n";
+                        salida += array[row, TAMX - 1];
                     }
+                    else if (!SelectRow)
+                    {
+                        for (int i = 1; i < TAMY; i++)
+                        {
+                            salida += $"{i}.- ";
+                            for (int j = 0; j < TAMX - 1; j++)
+                            {
+                                salida += array[i, j] + ", ";
+                            }
+                            salida += $"{array[i, TAMX - 1]}\n";
+                        }
+                    }
+                    MessageBox.Show(salida);
                 }
-                MessageBox.Show(salida);
+                else
+                    MessageBox.Show($"No existe matriz en la fila {row}");
             }
-            else
-                MessageBox.Show($"No existe matriz en la fila {row}");
+            catch (IndexOutOfRangeException) { MessageBox.Show("Fila no valida"); }
+            
         }
         /// <summary>
         /// Funcion que comprueba si una Matriz bidimensional tiene datos o no y lee una fila de la Matriz
@@ -204,17 +217,22 @@ namespace Ejercicio17
         int[] SeleccionarArray(int[,] array, int numArray)
         {
             int[] output = new int[TAMX];
-            if (array[numArray, 0] != 0)
+            try
             {
-                for (int i = 0; i < TAMX; i++)
-                    output[i] = array[numArray, i];
+                if (array[numArray, 0] != 0)
+                {
+                    for (int i = 0; i < TAMX; i++)
+                        output[i] = array[numArray, i];
+                }
+                else
+                {
+                    output[0] = -1;
+                    MessageBox.Show($"La matriz {numArray} esta vacia");
+                }
             }
-            else
-            {
-                output[0] = -1;
-                MessageBox.Show($"La matriz {numArray} esta vacia");
-            }
+            catch { }
             return output;
+            
         }
 
         //------------------------------Sistema de Seleccion de Operacion------------------------------//
@@ -222,16 +240,16 @@ namespace Ejercicio17
         /// <summary>
         /// Resalta el boton seleccionado
         /// </summary>
-        /// <param name="op">Valor de la operacion</param>
+        /// <param name="op">(float)Valor de la operacion</param>
         string SeleccionarBoton(float op)
         {
-            bSum.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            bSum.BackColor = System.Drawing.SystemColors.ControlLight;
             bSum.ForeColor = System.Drawing.SystemColors.ControlText;
-            bRes.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            bRes.BackColor = System.Drawing.SystemColors.ControlLight;
             bRes.ForeColor = System.Drawing.SystemColors.ControlText;
-            bMul.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            bMul.BackColor = System.Drawing.SystemColors.ControlLight;
             bMul.ForeColor = System.Drawing.SystemColors.ControlText;
-            bDiv.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            bDiv.BackColor = System.Drawing.SystemColors.ControlLight;
             bDiv.ForeColor = System.Drawing.SystemColors.ControlText;
             if (op == 0.1f)
             {
@@ -262,8 +280,8 @@ namespace Ejercicio17
         /// <summary>
         /// Especifica un nuevo texto en base al indice del objeto del formulario
         /// </summary>
-        /// <param name="indexBoton">Indice del objecto a buscar</param>
-        /// <param name="textoBoton">Texto a establecer en el objeto</param>
+        /// <param name="indexBoton">(int32)Indice del objecto a buscar</param>
+        /// <param name="textoBoton">(string)Texto a establecer en el objeto</param>
         void CambiarTextoBotones(int indexBoton, string textoBoton)
         {
             foreach (Control elementos in this.Controls)
@@ -275,6 +293,19 @@ namespace Ejercicio17
             }
         }
 
+        //------------------------------Sistema de Comprobacion de Arrays------------------------------//
+
+        bool MBSobreescribir(int lineaArray)
+        {
+            bool salida = true;
+            if (vglobal[lineaArray, 0] > 0)
+            {
+                DialogResult seleccion = MessageBox.Show("Sobreescribir los datos", $"Desea sobreescribir los datos de la linea{lineaArray}", MessageBoxButtons.YesNo);
+                if (seleccion == DialogResult.No) { salida = false; }
+            }
+            return salida;
+        }
+
         //------------------------------Sistema de Botones------------------------------//
 
         //Crear la Matriz//
@@ -283,14 +314,16 @@ namespace Ejercicio17
             try
             {
                 int WhereSave = int.Parse(txtSaveArray.Text);
-                var rand = new Random();
-                sorted = 0.0f;
-                for (int i = 0; i < TAMX; i++)
+                if (MBSobreescribir(WhereSave))
                 {
-                    vglobal[0, i] = rand.Next(50, 400);
+                    var rand = new Random();
+                    sorted = 0.0f;
+                    for (int i = 0; i < TAMX; i++)
+                    {
+                        vglobal[0, i] = rand.Next(50, 400);
+                    }
+                    EscribirArray(vglobal, WhereSave, vglobal);
                 }
-                EscribirArray(vglobal, WhereSave, vglobal);
-                MessageBox.Show($"Matriz de {TAMX} numeros ha sido creado con exito!");
             }
             catch (FormatException)
             {
@@ -323,99 +356,172 @@ namespace Ejercicio17
         //Busca un valor especifico en la Matriz//
         private void bLookItem_Click(object sender, EventArgs e)
         {
-            bool numFind = false;
-            int WhereRead = int.Parse(txtSaveArray.Text);
-            string salida = null;
-            if (vglobal[0, WhereRead] != -1)
+            try
             {
-                try
+                int WhereRead = int.Parse(txtReadArray.Text);
+                if (WhereRead < TAMY)
                 {
-                    int numLook = int.Parse(Interaction.InputBox("Ingresa el numero a buscar", "Buscador de numeros", "Ingresa un numero"));
-                    for (int i = 0; i < TAMX; i++)
+                    bool numFind = false;
+
+                    string salida = null;
+                    if (vglobal[0, WhereRead] != -1)
                     {
-                        if (vglobal[WhereRead, i] == numLook)
+                        try
                         {
-                            numFind = true;
-                            if (salida == null)
-                                salida += ($"El numero {numLook} se encuentra en:");
-                            salida += ($"\n La posicion {i + 1} (Indice {i})");
+                            int numLook = int.Parse(Interaction.InputBox("Ingresa el numero a buscar", "Buscador de numeros", "Ingresa un numero"));
+                            for (int i = 0; i < TAMX; i++)
+                            {
+                                if (vglobal[WhereRead, i] == numLook)
+                                {
+                                    numFind = true;
+                                    if (salida == null)
+                                        salida += ($"El numero {numLook} se encuentra en:");
+                                    salida += ($"\n La posicion {i + 1} (Indice {i})");
+                                }
+                            }
+                            if (numFind == false)
+                                MessageBox.Show("No se encuentra el numero");
                         }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Numero no valido, ingrese un numero valido para continuar");
+                        }
+                        if (numFind == true)
+                            MessageBox.Show(salida);
                     }
-                    if (numFind == false)
-                        MessageBox.Show("No se encuentra el numero");
                 }
-                catch (FormatException)
-                {
-                    MessageBox.Show("Numero no valido, ingrese un numero valido para continuar");
-                }
-                if (numFind == true)
-                    MessageBox.Show(salida);
+                else { throw new Exception("Linea no disponible"); }
             }
+            catch (FormatException)
+            {
+                MessageBox.Show("Ingrese una linea a analizar");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
         }
 
         //Ordena la Matriz ascendentemente//
         private void bSortArray_Click(object sender, EventArgs e)
         {
-            int WhereRead = int.Parse(txtReadArray.Text);
-            int WhereSave = int.Parse(txtSaveArray.Text);
-            if (sorted == 0.0f)
+            try
             {
-                ArrayBiToUni(ASS(SeleccionarArray(vglobal, WhereRead)));
-                ReverseArray(vglobal);
-                sorted = 0.1f;
+                int WhereRead = int.Parse(txtReadArray.Text);
+                if (MBSobreescribir(WhereRead))
+                {
+                    int WhereSave = int.Parse(txtSaveArray.Text);
+                    if (WhereRead > TAMY) { throw new Exception("Fila a leer no valida"); }
+                    if (WhereSave > TAMY) { throw new Exception("Fila a escribir no vaida"); }
+                    if (SeleccionarArray(vglobal, WhereRead)[0] != 0)
+                    {
+                        if (sorted == 0.0f)
+                        {
+                            ArrayBiToUni(ASS(SeleccionarArray(vglobal, WhereRead)));
+                            ReverseArray(vglobal);
+                            sorted = 0.1f;
+                        }
+                        if (sorted == 0.2f)
+                        {
+                            ReverseArray(vglobal);
+                            sorted = 0.1f;
+                        }
+                        EscribirArray(vglobal, WhereSave, vglobal);
+                        LeerArray(vglobal, true, WhereSave);
+                    }
+                    else { MessageBox.Show($"No existe array en la posicion {WhereRead}"); }
+                }
             }
-            if (sorted == 0.2f)
-            {
-                ReverseArray(vglobal);
-                sorted = 0.1f;
-            }
-            EscribirArray(vglobal, WhereSave, vglobal);
-            LeerArray(vglobal, true, WhereSave);
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
         }
 
         //Ordena la Matriz descendentemente//
         private void bSortArrayDes_Click(object sender, EventArgs e)
         {
-            int WhereRead = int.Parse(txtReadArray.Text);
-            int WhereSave = int.Parse(txtSaveArray.Text);
-            if (sorted == 0.0f)
+            try
             {
-                ArrayBiToUni(ASS(SeleccionarArray(vglobal, WhereRead)));
-                sorted = 0.2f;
+                int WhereRead = int.Parse(txtReadArray.Text);
+                if (MBSobreescribir(WhereRead))
+                {
+                    int WhereSave = int.Parse(txtSaveArray.Text);
+                    if (WhereRead > TAMY - 1) { throw new Exception("Fila a leer no valida"); }
+                    if (WhereSave > TAMY - 1) { throw new Exception("Fila a escribir no vaida"); }
+                    if (SeleccionarArray(vglobal, WhereRead)[0] != 0)
+                    {
+                        if (sorted == 0.0f)
+                        {
+                            ArrayBiToUni(ASS(SeleccionarArray(vglobal, WhereRead)));
+                            sorted = 0.2f;
+                        }
+                        if (sorted == 0.1f)
+                        {
+                            ReverseArray(vglobal);
+                            sorted = 0.2f;
+                        }
+                        EscribirArray(vglobal, WhereSave, vglobal);
+                        LeerArray(vglobal, true, WhereSave);
+                    }
+                    else { MessageBox.Show($"No existe array en la posicion {WhereRead}"); }
+                }
             }
-            if (sorted == 0.1f)
-            {
-                ReverseArray(vglobal);
-                sorted = 0.2f;
-            }
-            EscribirArray(vglobal, WhereSave, vglobal);
-            LeerArray(vglobal, true, WhereSave);
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
         }
 
         //Realiza una operacion con los datos de la Matriz 1//
         private void b1and2_Click(object sender, EventArgs e)
         {
-            int[,] resultado = new int[1,TAMX];
+            int[,] resultado = new int[1, TAMX];
             int[] fila1 = new int[TAMX];
             int[] fila2 = new int[TAMX];
-            int WhereSave = int.Parse(txtSaveArray.Text);
+            int WhereSave;
+            int TxtFila1;
+            int TxtFila2;
             string salida = "Resultado:\n";
-            fila1 = SeleccionarArray(vglobal, int.Parse(txtFila1.Text));
-            fila2 = SeleccionarArray(vglobal, int.Parse(txtFila2.Text));
-            switch (action)
+            try
             {
-                case 0.1f: { for (int i = 0; i < TAMX; i++) resultado[0,i] = fila1[i] + fila2[i]; break; };
-                case 0.2f: { for (int i = 0; i < TAMX; i++) resultado[0,i] = fila1[i] - fila2[i]; break; };
-                case 0.3f: { for (int i = 0; i < TAMX; i++) resultado[0,i] = fila1[i] * fila2[i]; break; };
-                case 0.4f: { for (int i = 0; i < TAMX; i++) resultado[0,i] = fila1[i] / fila2[i]; break; };
+                WhereSave = int.Parse(txtSaveArray.Text);
+                if (MBSobreescribir(WhereSave))
+                {
+                    try
+                    {
+                        TxtFila1 = int.Parse(txtFila1.Text);
+                        TxtFila2 = int.Parse(txtFila2.Text);
+                        if (TxtFila1 >= TAMY || TxtFila2 >= TAMY)
+                        {
+                            throw new Exception("Fila no valida");
+                        }
+                        fila1 = SeleccionarArray(vglobal, TxtFila1);
+                        fila2 = SeleccionarArray(vglobal, TxtFila2);
+                        switch (action)
+                        {
+                            case 0.1f: { for (int i = 0; i < TAMX; i++) resultado[0, i] = fila1[i] + fila2[i]; break; };
+                            case 0.2f: { for (int i = 0; i < TAMX; i++) resultado[0, i] = fila1[i] - fila2[i]; break; };
+                            case 0.3f: { for (int i = 0; i < TAMX; i++) resultado[0, i] = fila1[i] * fila2[i]; break; };
+                            case 0.4f: { for (int i = 0; i < TAMX; i++) resultado[0, i] = fila1[i] / fila2[i]; break; };
+                        }
+                        for (int num = 0; num < TAMX; num++)
+                            salida += $"{resultado[0, num]}, ";
+                        if (fila1[0] != -1 && fila2[0] != -1)
+                        {
+                            EscribirArray(resultado, WhereSave, vglobal);
+                            MessageBox.Show(salida);
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        MessageBox.Show("Fila no valida para operar");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
             }
-            for (int num = 0; num<TAMX; num++)
-                salida += $"{resultado[0, num]}, ";
-            if(fila1[0] != -1 && fila2[0] != -1)
+            catch (FormatException)
             {
-                EscribirArray(resultado, WhereSave, vglobal);
-                MessageBox.Show(salida);
-            }               
+                MessageBox.Show("Espacio de guardado no valido");
+            }
         }
 
         //Establece el texto del boton y cambia el operador
@@ -438,7 +544,6 @@ namespace Ejercicio17
                             case "bMul": { action = 0.3f; break; }
                             case "bDiv": { action = 0.4f; break; }
                         }
-                        SeleccionarBoton(action);
                         CambiarTextoBotones(8, $"Espacio {fila1} {SeleccionarBoton(action)} Espacio {fila2}");
                     }
                 }
