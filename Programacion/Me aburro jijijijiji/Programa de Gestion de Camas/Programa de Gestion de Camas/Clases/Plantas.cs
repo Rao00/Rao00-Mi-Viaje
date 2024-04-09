b26034f77b313e457754dc944e61dd68e5929847
Rao00 <carlesmorales.mcv00@gmail.com>
2024-04-09 17:19:37 +0200

    Revert "Merge branch 'main' of https://github.com/Rao00/Rao00-Mi-Viaje"
    This reverts commit bfec478e98c5e4ec91a345f6b62e6d3f370158c7, reversing
    changes made to 623b0d024a03480068c94664274c28ecd2a8b78a.

--- Programacion/Me aburro jijijijiji/Programa de Gestion de Camas/Programa de Gestion de Camas/Clases/Plantas.cs
+++ Programacion/Me aburro jijijijiji/Programa de Gestion de Camas/Programa de Gestion de Camas/Clases/Plantas.cs
@@ -12,18 +12,11 @@ namespace Programa_de_Gestion_de_Camas
         List<Servicio> listaServicios;
 
         private string nombrePlanta;
-        private int maxCamas;
 
         public string Nombre 
         {  
             get { return nombrePlanta; } 
             set { nombrePlanta = value; }
-        }
-
-        public int MaxCamas
-        {
-            get { return maxCamas; }
-            set { maxCamas = value; }
         }
 
         public int TotalCamas
@@ -39,7 +32,6 @@ namespace Programa_de_Gestion_de_Camas
         public Plantas(string Nombre)
         {
             nombrePlanta = Nombre;
-            maxCamas = 0;
             listaServicios = new List<Servicio>();
         }
 
