1d387c3b0aa870bee3d055523a03d5fc56cf881d
Rao00 <carlesmorales.mcv00@gmail.com>
2024-08-21 19:21:08 +0200

    a

---
+++ Programacion/Me aburro jijijijiji/Pacman/Pacman/Aritmetica.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman
+{
+    static class Aritmetica
+    {
+        public static int MediaAngulosPlanoCartesiano(List<int> angulos)
+        {
+            float x = 0;
+            float y = 0;
+
+            foreach (int angulo in angulos)
+            {
+                x += (float)Math.Cos(angulo * (Math.PI / 180));
+                y += (float)Math.Sin(angulo * (Math.PI / 180));
+            }
+
+            x /= angulos.Count;
+            y /= angulos.Count;
+
+            return (int)(Math.Atan2(y, x) * (180/Math.PI));
+        }
+    }
+}
---
+++ Programacion/Me aburro jijijijiji/Pacman/Pacman/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Pacman
+{
+    partial class fPacman
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            GLControl = new OpenTK.WinForms.GLControl();
+            SuspendLayout();
+            // 
+            // GLControl
+            // 
+            GLControl.API = OpenTK.Windowing.Common.ContextAPI.OpenGL;
+            GLControl.APIVersion = new System.Version(3, 3, 0, 0);
+            GLControl.Flags = OpenTK.Windowing.Common.ContextFlags.Default;
+            GLControl.IsEventDriven = true;
+            GLControl.Location = new System.Drawing.Point(24, 12);
+            GLControl.Name = "GLControl";
+            GLControl.Profile = OpenTK.Windowing.Common.ContextProfile.Core;
+            GLControl.Size = new System.Drawing.Size(762, 762);
+            GLControl.TabIndex = 0;
+            GLControl.Text = "glControl1";
+            GLControl.Load += LoadGlControl;
+            GLControl.Paint += PaintGLControl;
+            GLControl.KeyDown += KeyPressed;
+            // 
+            // fPacman
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            AutoSize = true;
+            BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            ClientSize = new System.Drawing.Size(809, 786);
+            Controls.Add(GLControl);
+            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            Name = "fPacman";
+            Text = "Pacman";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private OpenTK.WinForms.GLControl GLControl;
+    }
+}
+
